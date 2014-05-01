76b2fd8223bb5ba2b6c24db37eba3c454b07cf25
KalyanEvoke <kveerlapati@evoketechnologies.com>
2014-05-01 22:10:41 +0530

    added admin controller

---
+++ MvcPoc/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using MvcPoc.Web.DAL.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+namespace MvcPoc.Web.Controllers
+{
+    [CustomAuthorize(RolesConfigKey = "RolesConfigKey")]
+    // [CustomAuthorize(UsersConfigKey = "UsersConfigKey")]
+    //[CustomAuthorize(Roles = "Admin")]
+    // [CustomAuthorize(Users = "1")]
+    public class AdminController : BaseController
+    {
+        //
+        // GET: /Admin/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Logoff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Account");
+        }
+    }
+}
