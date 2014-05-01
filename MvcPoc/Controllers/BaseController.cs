10fb9322aaaa61573e1cd85e7be058fd24b0dfd3
KalyanEvoke <kveerlapati@evoketechnologies.com>
2014-05-01 22:10:53 +0530

    base controller

---
+++ MvcPoc/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcPoc.Web.DAL.Security;
+using MvcPoc.Web.DAL;
+namespace MvcPoc.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected virtual new CustomPrincipal User
+        {
+            get { return HttpContext.User as CustomPrincipal;  }
+        }
+    }
+}
